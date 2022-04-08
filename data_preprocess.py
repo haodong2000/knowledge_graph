c9044f81af37a9435b2285f997019e03b8133888
LeBronLiHD <lebronlihd@outlook.com>
2022-04-08 21:48:26 +0800

    update data and data_preprocess

--- data_preprocess.py
+++ data_preprocess.py
@@ -5,16 +5,29 @@ from openpyxl import load_workbook
 from xlwt import Workbook
 
 
-def create_sheet(sheet_name, savename, tran_dict, ori_data):
+def no_space(name):
+    no_space = ""
+    for c in name:
+        if c == ' ':
+            no_space += '_'
+        else:
+            no_space += c
+    return no_space
+
+def create_sheet(sheet_name, savename, tran_dict, ori_data, type_dict):
     wb = Workbook()
     data_sheet = wb.add_sheet(sheet_name)
     data_sheet.write(0, 0, "subject")
-    data_sheet.write(0, 1, "relation")
-    data_sheet.write(0, 2, "object")
-    for i in range(len(ori_data[ori_data.columns[0]])):
-        data_sheet.write(i + 1, 0, tran_dict[ori_data[ori_data.columns[0]][i]])
-        data_sheet.write(i + 1, 1, ori_data[ori_data.columns[1]][i])
-        data_sheet.write(i + 1, 2, tran_dict[ori_data[ori_data.columns[2]][i]])
+    data_sheet.write(0, 1, "sub_type")
+    data_sheet.write(0, 2, "relation")
+    data_sheet.write(0, 3, "object")
+    data_sheet.write(0, 4, "obj_type")
+    for i in range(len(ori_data)):
+        data_sheet.write(i + 1, 0, no_space(tran_dict[ori_data[ori_data.columns[0]][i]]))
+        data_sheet.write(i + 1, 1, no_space(type_dict[tran_dict[ori_data[ori_data.columns[0]][i]]]))
+        data_sheet.write(i + 1, 2, no_space(ori_data[ori_data.columns[1]][i]))
+        data_sheet.write(i + 1, 3, no_space(tran_dict[ori_data[ori_data.columns[2]][i]]))
+        data_sheet.write(i + 1, 4, no_space(type_dict[tran_dict[ori_data[ori_data.columns[2]][i]]]))
     wb.save(savename)
     print("sheet", sheet_name, "created")
 
@@ -24,13 +37,14 @@ def read_data(filename, sheet_name="node"):
     return pd.read_excel(filename, sheet_name=sheet_name)
 
 
-def build_dictionary(data, cn_label, en_label):
-    cn_names, en_names = data[cn_label], data[en_label]
+def build_dictionary(data, cn_label, en_label, type_label):
+    cn_names, en_names, types = data[cn_label], data[en_label], data[type_label]
     print("length of names ->", len(cn_names), len(en_names))
-    dictionary = {}
+    tran_dict, type_dict = {}, {}
     for i in range(len(cn_names)):
-        dictionary[cn_names[i]] = en_names[i]
-    return dictionary
+        tran_dict[cn_names[i]] = en_names[i]
+        type_dict[en_names[i]] = types[i]
+    return tran_dict, type_dict
 
 
 if __name__ == "__main__":
@@ -38,7 +52,8 @@ if __name__ == "__main__":
     data = read_data(filename)
     cn_label = "ChineseName"
     en_label = "EnglishName"
-    tran_dict = build_dictionary(data, cn_label, en_label)
+    type_label = "Type"
+    tran_dict, type_dict = build_dictionary(data, cn_label, en_label, type_label)
     savename = "./data/en_relation.xls"
     ori_data = read_data(filename, sheet_name="relation")
-    create_sheet("en_relation", savename, tran_dict, ori_data)
+    create_sheet("en_relation", savename, tran_dict, ori_data, type_dict)
